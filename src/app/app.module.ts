22c8dc15396e1074f20f0b2428d60ac028ef5e63
Ankita <ankita.sahu2010@gmail.com>
2022-07-18 19:17:08 +0530

    Payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderComponent } from './order/order.component';
 import { FurnitureManagementComponent } from './furniture-management/furniture-management.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderComponent,
-    FurnitureManagementComponent
+    FurnitureManagementComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
