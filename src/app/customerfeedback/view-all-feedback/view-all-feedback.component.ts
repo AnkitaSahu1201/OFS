2e9fcb19e6cf0a9b30598e385da7b8d1dcb0ecac
Ankita <ankita.sahu2010@gmail.com>
2022-07-19 15:57:38 +0530

    view feedback

---
+++ src/app/customerfeedback/view-all-feedback/view-all-feedback.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Review } from 'src/app/model/review';
+import { CustomerfeedbackService } from '../customerfeedback.service';
+
+@Component({
+  selector: 'app-view-all-feedback',
+  templateUrl: './view-all-feedback.component.html',
+  styleUrls: ['./view-all-feedback.component.css']
+})
+export class ViewAllFeedbackComponent implements OnInit {
+
+  public rev:Review=new Review();
+  viewlist:boolean=false;
+  constructor(private service:CustomerfeedbackService) { 
+  }
+  listrev:Review[];
+  ngOnInit(): void {
+  }
+  viewAllReview():void{
+    this.service.getAllReviews().subscribe((r)=>{this.listrev=r,this.viewlist=true});
+  }
+  // viewAllReviewRating():void{
+  //   this.service.getReviewByreviewRating(reviewRating).subscribe((r)=>{this.listrev=r,this.viewlist=true});
+  // }
+ 
+ 
+  
+
+}
